c32c087849b04f5c8df018883dca291389c01e89
Yuliya005 <mazi666@mail.ru>
2023-05-02 16:25:01 +0300

    correction1

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
                 String bob = "Bob";
-                Scanner sc = new Scanner(System.in);
+                Scanner scan = new Scanner(System.in);
                 System.out.print("Введите ваше имя ");
-                String name = sc.nextLine();
+                String name = scan.nextLine();
                 System.out.print("Введите ваш возраст");
-                short age = sc.nextShort();
+                short age = scan.nextShort();
                 System.out.print("Введите желаемую сумму кредита");
-                int sum = sc.nextInt();
+                int sum = scan.nextInt();
 
                 if (!name.equalsIgnoreCase(bob) && (age >= 18) && (sum <= (age * 100))) {
                     System.out.println("Кредит одобрен");
