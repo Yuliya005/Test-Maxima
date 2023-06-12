ab4b107708bfc53aabfd48241d1d571df2a9d900
Yuliya005 <129603174+Yuliya005@users.noreply.github.com>
2023-06-12 21:57:22 +0300

    Delete Square.java

--- Square.java
+++
@@ -1,9 +0,0 @@
-public class Square extends GeometricShapes {
-    public double sideA;
-
-    public Square(double sideA) {
-        this.sideA = sideA;
-    }
-    public void setSideA(double sideA) {
-        this.sideA = sideA;
-    }}
