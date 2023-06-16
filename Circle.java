fcbe878395b148c566a882cc235f000681be4020
Yuliya005 <mazi666@mail.ru>
2023-06-16 14:00:35 +0300

    классы,наследование

--- Circle.java
+++ Circle.java
@@ -1,17 +1,28 @@
-public class Circle extends GeometricShapes{
+public class Circle extends Figure {
+    private static final String NAME = "Circle";
 
-    public final double PI = 3.14;
-    public double radius;
-    public Circle() {
-    }
+    private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
     }
-    public void setRadius(double radius){
+
+    @Override
+    public double getArea(){
+        double area = Math.PI * radius radius;
+        return area;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
         this.radius = radius;
     }
-    @Override
-    public double getArea() {
-        return (PI * (radius*radius));
-    }  }
+}
